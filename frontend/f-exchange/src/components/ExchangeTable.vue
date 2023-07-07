02b5e28919b25e27e8631575a64a0538d7b38d6d
peter <pjmakey2@gmail.com>
2023-07-07 00:22:20 -0400

    * Delete webdatarocks package * Create Table component to show the currencies * Add basic css style to the table component

---
+++ frontend/f-exchange/src/components/ExchangeTable.vue
@@ -0,0 +1,30 @@
+<template>
+    <table>
+      <thead>
+        <tr>
+          <th>Source</th>
+          <th>Currency</th>
+          <th>Buy</th>
+          <th>Sale</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in currency_data" :key="row.id">
+          <td>{{ row.source }}</td>
+          <td>{{ row.currency }}</td>
+          <td>{{ row.buy }}</td>
+          <td>{{ row.sale }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+<script setup>
+    import { ref } from 'vue';
+    const props = defineProps({
+        currencies: {type: Array, required: true },
+        edate: String
+    })
+
+    const currency_data = [{source: 'SET', currency: 'USD', buy: '1500', sale: '15000'}];
+
+</script>
--- frontend/f-exchange/src/views/ExchangeView.vue
+++ frontend/f-exchange/src/views/ExchangeView.vue
@@ -1,12 +1,16 @@
 <script setup>
+import { ref } from 'vue';
 import ExchangeChart from '../components/ExchangeChart.vue';
-import PivotExchange from '../components/PivotExchange.vue';
+import ExchangeTable from '../components/ExchangeTable.vue';
 
+const currencies = ref(['USD']);
 
 </script>
 <template>
     <ExchangeChart></ExchangeChart>
     <hr>
-    <h2 class="title">GRID - Currency data of PY</h2>
-    <PivotExchange></PivotExchange>
+    <h2 class="title">Grid Exchange of the day</h2>
+    <ExchangeTable
+    :currencies="currencies"
+    ></ExchangeTable>
 </template>
